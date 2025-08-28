7df7ddfb3f3328cb5ad437c80c76a4845277ea63
ziikuu <angelo.bringula@gmail.com>
2025-08-29 02:26:56 +0800

    update NavBar.tsx: made the <a> tag in the Logo (Computer Engineer) a <Link> tag from react it handles prefetching so that the hand cursor will show up when you hover.

--- components/Header/NavBar.tsx
+++ components/Header/NavBar.tsx
@@ -12,12 +12,12 @@ const NavBar = ({ openMenu }: NavBarProps) => {
     <div id="navContainer" className='sticky top-0 z-1000 flex items-center justify-between px-4 py-4 font-[DM_Mono] text-lg'>
       {/* Logo or Brand Name */}
       <div className=''>
-        <a href='/'><p>Computer Engineer</p></a>
+        <Link href='/'><p>Computer Engineer</p></Link>
       </div>
       {/* Navigation Links for Desktop */}
       <div id='navLinks' className='hidden md:flex gap-10'>
         {NavLinks.map((link) => (
-          <Link key={link.label} href={link.url} className='ease-in-out after:content-[""] after:block after:w-full after:h-[2px] after:bg-[#151419] after:scale-x-0 hover:after:scale-x-100 after:origin-right hover:after:origin-left after:transition-transform after:duration-300'>
+          <Link key={link.label} href={link.url} className='ease-in-out after:content-[""] after:block after:w-full after:h-[2px] after:bg-[#151419] after:scale-x-0 hover:after:scale-x-100 after:origin-right hover:after:origin-left after:transition-transform after:duration-300 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out active:scale-95'>
             <div className='flex flex-col items-center justify-center'>
               <p>{link.label}</p>
             </div>
