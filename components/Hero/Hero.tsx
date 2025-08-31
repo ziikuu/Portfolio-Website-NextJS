1439a00cb075d317b21262b2dfce67cc1979b6c9
ziikuu <angelo.bringula@gmail.com>
2025-08-31 18:02:33 +0800

    update Hoero.tsx: Changed the h1 font to Playfair_Display_SC. optimized some fonts in the hero. styled the button properly and added another  button for about me page.

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Source_Serif_4 } from 'next/font/google'
+import { Playfair_Display_SC } from 'next/font/google'
 
-const sourceSerif4 = Source_Serif_4({
+const PlayfairDisplaySC = Playfair_Display_SC({
   subsets: ["latin"],
-  weight: ["600"], 
-  variable: '--font-source-serif-4'
+  weight: ["700"], 
 });
 
 const Hero = () => {
@@ -16,18 +15,23 @@ const Hero = () => {
         {/* Text Content */}
         <div className="hero-text w-[312px] md:w-1/2  flex flex-col items-start justify-center min-w-[312px]">
           <div className="flex">
-            <h1 id="name" className={`${sourceSerif4.className} text-[clamp(4rem,10vw,9rem)] m-auto`}>
+            <h1 id="name" className={`${PlayfairDisplaySC.className} text-[clamp(3.8rem,9vw,9rem)] m-auto`}>
               Angelo Bringula
             </h1>
           </div>
           <div className="flex flex-col items-start  md:max-w-full">
-            <p className="text-[clamp(0.9rem,1.2vw,1.5rem)] max-w-3/4 md:max-w-3/4 text-justify font-['DM_Mono_Variable'] leading-relaxed">
+            <p className="text-[clamp(0.9rem,1.2vw,1.5rem)] max-w-3/4 md:max-w-3/4 text-justify font-[DM_Mono] leading-relaxed">
               I'm a computer engineering graduate from the Philippines, passionate about becoming a software or web developer.
             </p>
-            <div className=''>
+            <div className='inline-flex'>
               <Link href="/Portfolio" className=''>
-                  <button className="mt-4 px-8 py-3 bg-[#151419] text-white rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out">
+                  <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 px-4 py-3 bg-[#151419] text-white rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
                     <p>See My Projects</p>
+                  </button>
+              </Link>
+              <Link href="/About" className='ml-4'>
+                  <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 px-4 py-3 bg-transparent border-2 border-[#151419] text-[#151419] rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
+                    <p>More About Me</p>
                   </button>
               </Link>
             </div>
