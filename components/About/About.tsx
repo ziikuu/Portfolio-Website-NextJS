2a588b08e793521fc8673d7a58a4f4a5168a48ea
ziikuu <angelo.bringula@gmail.com>
2025-09-01 16:32:19 +0800

    feat(About, Hero): integrate Balancer for improved text wrapping and add background blur effects

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Playfair_Display_SC } from 'next/font/google'
 import Link from 'next/link'
+import Balancer from 'react-wrap-balancer';
 
 const PlayfairDisplaySC = Playfair_Display_SC({
   subsets: ["latin"],
@@ -9,19 +10,25 @@ const PlayfairDisplaySC = Playfair_Display_SC({
 
 function About() {
   return (
-    <section id="hero">
+    <section id="hero" className='bg-[#D9D9D9]'>
       <div className="flex flex-col justify-center items-center min-h-screen p-8 relative z-10">
+        {/* Background Blurs */}
+        <div className='absolute top-0 left-0 w-full h-full -z-10 overflow-hidden'>
+          <div className='absolute blur-[130px] opacity-80 bg-[#4F504E] rounded-full top-20 -left-100 h-150 w-150'></div>
+          <div className='absolute blur-[130px] opacity-80 bg-[#4F504E] rounded-full top-90 -right-100 h-150 w-150'></div>
+        </div>
         {/* Title */}
+        <Balancer>
         <div className='flex flex-col justify-center items-center w-full mb-8'>
           <h1 className={`text-[#151419] text-[clamp(3.8rem,9vw,9rem)] ${PlayfairDisplaySC.className}`}>
-            About Page
+            About Me
           </h1>
           <div className='bg-[#151419] w-full h-1'></div>
         </div>
         {/* Description */}
         <div className='font-[IBM_Plex_Sans] text-[#151419] text-[clamp(0.9rem,1.5vw,1.5rem)] text-justify w-full  xl:max-w-4xl xl:w-4xl lg:w-2xl md:w-lg sm:w-sm'>
           <p className=''>
-            I’m a Computer Engineering graduate and aspiring full-stack developer from the Philippines. Skilled in HTML, CSS, React, Nuxt, C++, C#, and Python, I enjoy building web apps and automation systems.
+            I’m a Computer Engineering graduate and aspiring full-stack developer from the Philippines. Skilled in HTML, CSS, React, Nuxt, Next.js, C++, C#, and Python, I enjoy building web apps and automation systems.
           </p>
           <p className='pt-4'>
             I’ve led a machine learning–based research project that automated crop cultivation for farmers, and I graduated cum laude from the University of the East. Guided by creativity and integrity, I aim to grow into a senior developer and automation engineer who builds solutions that make life and work easier.
@@ -29,17 +36,20 @@ function About() {
         </div>
         {/* Buttons */}
         <div className='inline-flex'>
-          <Link href="/Portfolio" className=''>
-              <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 px-4 py-3 border-2 border-[#151419] bg-[#151419] text-white rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
-                <p>Download My CV</p>
-              </button>
-          </Link>
-          <Link href="/About" className='ml-4'>
-              <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 px-4 py-3 bg-transparent border-2 border-[#151419] text-[#151419] rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
-                <p>See My Portfolio</p>
-              </button>
-          </Link>
+          <div className='flex justify-start items-start'>
+            <Link href="/Portfolio" className=''>
+                <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 p-3 border-2 border-[#151419] bg-[#151419] text-white rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
+                  <p>Download My CV</p>
+                </button>
+            </Link>
+            <Link href="/About" className='ml-4'>
+                <button className="text-[clamp(0.8rem,1.2vw,1.5rem)] mt-4 p-3 bg-transparent border-2 border-[#151419] text-[#151419] rounded-full font-[DM_Mono] hover:transform hover:-translate-y-0.5 hover:shadow-lg transition-all duration-300 ease-in-out active:scale-95 active:opacity-70 active:transition-all active:duration-75 active:ease-in-out">
+                  <p>See My Portfolio</p>
+                </button>
+            </Link>
+          </div>
         </div>
+        </Balancer>
       </div>
     </section>
   )
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Playfair_Display_SC } from 'next/font/google'
+import Balancer from 'react-wrap-balancer';
 
 const PlayfairDisplaySC = Playfair_Display_SC({
   subsets: ["latin"],
@@ -11,7 +12,12 @@ const PlayfairDisplaySC = Playfair_Display_SC({
 const Hero = () => {
   return (
       <section id="hero" className="flex flex-col-reverse md:flex-row justify-center items-center min-h-screen p-8 relative z-10">
+        <div className='absolute top-0 left-0 w-full h-full -z-10 overflow-hidden'>
+          <div className='absolute blur-[130px] opacity-80 bg-[#4F504E] rounded-full top-20 -left-100 h-150 w-150'></div>
+          <div className='absolute blur-[130px] opacity-80 bg-[#4F504E] rounded-full top-90 -right-100 h-150 w-150'></div>
+        </div>
         {/* Text Content */}
+        <Balancer>
         <div className="hero-text w-[312px] md:w-1/2  flex flex-col items-start justify-center min-w-[312px]">
           <div className="flex">
             <h1 id="name" className={`${PlayfairDisplaySC.className} text-[#151419] text-[clamp(3.8rem,9vw,9rem)] m-auto`}>
@@ -19,7 +25,7 @@ const Hero = () => {
             </h1>
           </div>
           <div className="flex flex-col items-start  md:max-w-full">
-            <p className="text-[#151419] text-[clamp(0.9rem,1.2vw,1.5rem)] max-w-3/4 md:max-w-3/4 text-justify font-[DM_Mono] leading-relaxed">
+            <p className="text-[#151419] text-[clamp(0.9rem,1.2vw,1.5rem)] text-justify font-[DM_Mono] leading-relaxed">
               I'm a computer engineering graduate from the Philippines, passionate about becoming a software or web developer.
             </p>
             <div className='inline-flex'>
@@ -36,6 +42,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
+        </Balancer>
 
         {/* Image Content */}
         <div className="w-full md:w-1/2 flex justify-center items-center">
