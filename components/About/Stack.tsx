0947d37679e7ed9eb24ba0bcc043c79feba8a0f2
ziikuu <angelo.bringula@gmail.com>
2025-09-04 01:21:10 +0800

    update Stack.tsx: fixed the stack title design

--- components/About/Stack.tsx
+++ components/About/Stack.tsx
@@ -7,27 +7,18 @@ function Stack() {
   return (
     <div id="stack" className='flex flex-col justify-center items-center min-h-screen p-8 relative z-10 bg-[#D9D9D9] bg-cover bg-no-repeat bg-fixed w-full'>
         <BgBlur />
-        {/* Content Container */}
-      
-      <div id='stackContainer' className='flex flex-row justify-center items-center w-fit p-8 relative z-10 gap-4'>
-        <div id='titleContainer' className='text-right font-[playfair_display_sc] font-black text-[#151419] mb-4'>
-            <h1 className={`text-[2rem]`}>My<br />Tech<br />Stack</h1>
+        <div id='stackContainer' className='flex flex-row justify-center items-center w-fit p-8 relative z-10 gap-4'>
+          <div id='titleContainer' className='text-right font-[playfair_display_sc] font-black text-[#151419] mb-4'>
+              <h1 className={`text-[2rem] sm:text-[2.7rem]`}>My<br />Tech<br />Stack</h1>
+          </div>
+       
+          <div id='SubtitleContainer' className='font-[playfair_display_sc] font-normal text-[#151419] text-left whitespace-pre-line max-w-[200px] sm:max-w-[300px] min-w-[128px]'>
+            <p className='text-[clamp(0.65em,2vw,1em)]'>
+              {"These include,\nbut are not limited to,\nthe technologies I use\nto build websites\nand develop software"}
+            </p>
+          </div>
         </div>
-     
-        <div id='SubtitleContainer' className='font-[playfair_display_sc] font-normal text-[#151419] text-left '>
-          <p className={`text-[0.8rem]`}>
-            These include,<br />  
-            but are not limited to, <br />
-            the technologies I use <br />
-            to build websites <br />
-            and develop software
-          </p>
-        </div>
-    
-      </div>
-      
         {/* SVG Stack Image */}
-        
     </div>
   )
 }
