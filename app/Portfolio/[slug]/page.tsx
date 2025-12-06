2ef808b3dddef88bd420eecbc1d0e0088007dd25
ziikuu <angelo.bringula@gmail.com>
2025-12-07 00:11:56 +0800

    added slug for dynammic pages and tested the dynamic segment foro multiple portfolio project title

---
+++ app/Portfolio/[slug]/page.tsx
@@ -0,0 +1,12 @@
+export default async function ProjectsPage({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}) {
+    const { slug } = await params
+    return (
+        <section>
+            <h1>{slug}</h1>
+        </section>
+    )
+}
