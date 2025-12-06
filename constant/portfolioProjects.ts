45df543ac7bbc532bc3ed2e633c4f8e09f0cc8aa
ziikuu <angelo.bringula@gmail.com>
2025-12-07 00:21:32 +0800

    refactored Project_card.tsx: moved the projects component in the constant folder and added a file called portfolioProjects.ts to store all the portfolio project metadata

---
+++ constant/portfolioProjects.ts
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+      title: "Smart Hydroponics: IoT-Powered Lettuce Cultivation",
+      url: "https://github.com/your-username/smart-hydroponics",
+      description:
+        "Led development of an IoT-powered hydroponics system with ML-driven predictive analytics. Improved lettuce yield by 10% and reduced manual labor by 77 hours per week through automated monitoring and real-time data insights.",
+      image: "/projects/Hydrponics-Hardware.jpg",
+      techStack: ["React", "Arduino Cloud", "C++", "TensorFlow", "Python", "Flask", "Firebase", "ESP32", "Docker"],
+      imagePosition: "object-center",
+      id: "hydroponics",
+    },
+    {
+      title: "Avelo Jewelry Repair Shop - Portfolio Website",
+      url: "https://github.com/your-username/smart-hydroponics",
+      description:
+        "I designed and developed a responsive portfolio website to showcase Avelo Jewelry Workshop’s custom creations, making it easier for clients to browse designs and find inspiration for personalized orders. The project highlighted the craftsmanship of the brand while boosting engagement by providing an accessible digital showcase for potential customers.",
+      image: "/projects/Avelo-Jewelry-Portfolio.webp",
+      techStack: ["Next.js", "Tailwind CSS", "Git", "GitHub", "Vercel"],
+      imagePosition: "object-center",
+      reverse: true,
+      id: "avelo-jewelry",
+    },
+    {
+      title: "Job Application Platform",
+      url: "https://your-job-platform.vercel.app",
+      description:
+        "Project Lead for a full-stack job application platform. Managed cross-functional team through complete development lifecycle, integrating front-end and back-end systems with enhanced performance and usability.",
+      image: "/projects/CoESS-LinkUp_Hero.png",
+      techStack: ["Nuxt.js", "Vue.js", "Git", "GitHub", "Vercel"],
+      id: "job-application",
+    },
+    {
+      title: "Partner School Contact Tracking System",
+      url: "https://github.com/your-username/contact-tracker",
+      description:
+        "Developed during internship at University of the East to increase marketing outreach by 20%. Built mobile application to efficiently track contact details of 160+ partner schools, streamlining communication processes.",
+      image: "/projects/CareerTalks_contacts.png",
+      techStack: ["React Native", "Firebase", "JavaScript", "Mobile Development"],
+      reverse: true,
+      id: "contact-tracker",
+    },
+  ]
