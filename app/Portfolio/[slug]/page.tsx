e1fe8c404ddf193e98b410bb71c434341642c8f9
ziikuu <angelo.bringula@gmail.com>
2025-12-08 23:33:07 +0800

    laid the founddation for the dynamic route for each project page. Uses slug to generate dynamic routes for each project page. The code at app/Portfolio/[slug]/page.tsx generates metadata for each project page. Each project page is generated at build time for faster load.

--- app/Portfolio/[slug]/page.tsx
+++ app/Portfolio/[slug]/page.tsx
@@ -1,14 +1,36 @@
-{/*import { projects } from "@/constant/portfolioProjects"*/}
+import { projects } from "@/constant/portfolioProjects"
+import { Metadata } from "next";
+import { FallbackMode } from "next/dist/lib/fallback";
 
-export default async function ProjectsPage({
-    params,
-}: {
-    params: Promise<{ slug: string }>
-}) {
-    const { slug } = await params
+{/* Data fetch function */}
+export async function getProjectBySlug(slug: string) {
+    return projects.find((p) => p.id === slug);     {/* matches the id of the project to the slug link */}
+}
+
+{/* Generate the metadata for each page */}
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }>
+}): Promise<Metadata> {
+    const { slug } = await params;
+    const project = await getProjectBySlug(slug);
+    return{
+        title: project?.title,
+        description: project?.description,
+    };
+}
+
+{/* Generate static slug project pages at build time */}
+export async function generateStaticParams() {
+    return projects.map((project) => ({slug: project.id}))
+}
+
+export default async function ProjectsPage({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+    const project = await getProjectBySlug(slug);
+
     return (
-        <section>
-            <h1>{slug}</h1>
-        </section>
-    )
+        <article>
+            <h1>{project?.title}</h1>
+            <p>{project?.description}</p>
+        </article>
+    );
 }
